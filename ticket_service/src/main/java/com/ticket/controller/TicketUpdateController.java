9547ea1e9900d77d4b016cf0e3a093ba76c1b2df
Şeyda Südüpak <ssudupak711@gmail.com>
2025-07-09 10:03:42 +0300

    TicketUpdateController eklendi

--- ticket_service/src/main/java/com/ticket/controller/TicketUpdateController.java
+++ ticket_service/src/main/java/com/ticket/controller/TicketUpdateController.java
@@ -1,5 +1,25 @@
 package com.ticket.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ticket.dto.TicketResponse;
+import com.ticket.dto.TicketUpdateRequest;
+import com.ticket.service.ITicketUpdateService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class TicketUpdateController {
+
+    private final ITicketUpdateService ticketUpdateService;
+
+    @PostMapping("ticket/{id}")
+    public TicketResponse updateTicket(Long id, TicketUpdateRequest request) {
+        return ticketUpdateService.updateTicket(id, request);
+    }
 
 }
