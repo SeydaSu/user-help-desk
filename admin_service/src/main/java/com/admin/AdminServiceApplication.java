e0651354057f34b0a63b70f107a37f8e07cde769
Şeyda Südüpak <ssudupak711@gmail.com>
2025-07-31 15:12:06 +0300

    feat(admin): update AdminServiceApplication

--- admin_service/src/main/java/com/admin/AdminServiceApplication.java
+++ admin_service/src/main/java/com/admin/AdminServiceApplication.java
@@ -2,9 +2,13 @@ package com.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.admin")
+@EnableJpaRepositories(basePackages = "com.admin.repository")
+@EntityScan(basePackages = "com.admin.model")
 @EnableFeignClients
 public class AdminServiceApplication {
 
