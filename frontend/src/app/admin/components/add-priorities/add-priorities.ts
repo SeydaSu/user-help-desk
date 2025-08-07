7de941062baba52633979188ed35aab80064c2fd
Şeyda Südüpak <ssudupak711@gmail.com>
2025-08-07 14:14:59 +0300

    feat(admin): add add-priorities component

---
+++ frontend/src/app/admin/components/add-priorities/add-priorities.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AdminFacade } from '../../store/admin.facade';
+import { Priority } from '../../../models/priority.model'
+
+@Component({
+  selector: 'app-add-priorities',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './add-priorities.html',
+  styleUrl: './add-priorities.css'
+})
+export class AddPriorities implements OnInit {
+  priorityForm: FormGroup;
+  priorities$: Observable<Priority[]>;
+
+  constructor(
+    private fb: FormBuilder,
+    private adminFacade: AdminFacade
+  ) {
+    this.priorityForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]]
+    });
+    this.priorities$ = this.adminFacade.priorities$;
+  }
+
+  ngOnInit(): void {
+    this.adminFacade.refreshPriorities();
+  }
+
+  onSubmit(): void {
+    if (this.priorityForm.valid) {
+      const priorityRequest = {
+        name: this.priorityForm.value.name.trim()
+      };
+      
+      this.adminFacade.createPriority(priorityRequest).subscribe({
+        next: () => {
+          this.priorityForm.reset();
+        },
+        error: (error) => {
+          console.error('Öncelik ekleme hatası:', error);
+        }
+      });
+    }
+
+}
+}
