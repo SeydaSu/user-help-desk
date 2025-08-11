f64af6dd67cec9bd3f30926068a52d9db76ae945
Şeyda Südüpak <ssudupak711@gmail.com>
2025-08-11 10:34:34 +0300

    feat(admin): enhance add-priority component

--- frontend/src/app/admin/components/add-priorities/add-priorities.ts
+++ frontend/src/app/admin/components/add-priorities/add-priorities.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { AdminFacade } from '../../store/admin.facade';
-import { Priority } from '../../../models/priority.model'
+import { Priority } from '../../../models/priority.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-priorities',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-priorities.html',
-  styleUrl: './add-priorities.css'
+  styleUrl: './add-priorities.css',
 })
 export class AddPriorities implements OnInit {
   priorityForm: FormGroup;
   priorities$: Observable<Priority[]>;
 
-  constructor(
-    private fb: FormBuilder,
-    private adminFacade: AdminFacade
+  constructor(private fb: FormBuilder, private adminFacade: AdminFacade,
+              private router: Router
   ) {
     this.priorityForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]]
+      name: ['', [Validators.required, Validators.minLength(2)]],
     });
     this.priorities$ = this.adminFacade.priorities$;
   }
@@ -33,18 +38,20 @@ export class AddPriorities implements OnInit {
   onSubmit(): void {
     if (this.priorityForm.valid) {
       const priorityRequest = {
-        name: this.priorityForm.value.name.trim()
+        name: this.priorityForm.value.name.trim(),
       };
-      
+
       this.adminFacade.createPriority(priorityRequest).subscribe({
         next: () => {
           this.priorityForm.reset();
         },
         error: (error) => {
           console.error('Öncelik ekleme hatası:', error);
-        }
+        },
       });
     }
-
-}
+  }
+  goBack() {
+    this.router.navigate(['/admin-dashboard']);
+  }
 }
